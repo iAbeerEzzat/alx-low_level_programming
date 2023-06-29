c52164c07a75bb4c28f211d2c86c416ad71bcd5e
Abeer.Ezzat <abeerezzat2312@gmail.com>
2023-06-29 12:34:17 -0700

    message

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ *
+ * Description: prints a[2] = 98, followed by a new line.
+ *
+ * Return: always 0 (success)
+*/
+
 int main(void)
 {
 	int n;
@@ -16,7 +24,7 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  *(p + 5) = 98;
+	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
