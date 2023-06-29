2371501e20b6242d907fb3a3775e3f0979b9ac8e
Abeer.Ezzat <abeerezzat2312@gmail.com>
2023-06-29 12:45:25 -0700

    message

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
 * leet - function that encode a string
-*@str:string that will be encoded
-*Return:returns encoded string
+* @str: string that will be encoded
+* Return: returns encoded string
 */
 
 char *leet(char *str)
