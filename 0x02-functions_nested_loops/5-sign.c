b4a29ea1cee2ed5594418507632a29c42cd1cf2c
Abeer.Ezzat <abeerezzat2312@gmail.com>
2023-06-29 09:36:52 -0700

    message

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,12 +9,12 @@
  *
  * Return : 1 if +, 0 if 0, -1 if -
 */
-int (print_sign(int n))
+int print_sign(int n)
 {
 	if (n > 0)
 	{
 		putchar('+');
-		return (1);
+		return (+1);
 	}
 	else if (n == 0)
 	{
